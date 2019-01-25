2e801e1d045bc8e9d0b8a27bb4b88c4f040d81b9
Craig Childs <childscraig17@gmail.com>
2019-01-25 15:59:32 +0000

    Pull in Cashier, add migration for it

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,9 +7,11 @@ use App\Models\UserProfile;
 use App\Models\UserTwoFactorToken;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Cashier\Billable;
 
 class User extends Authenticatable
 {
+    use Billable;
     use Notifiable;
 
     protected $guard = 'api';
