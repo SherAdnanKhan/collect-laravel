86416f94b191ed427cd50b57a92cbb35d6432422
Craig Childs <childscraig17@gmail.com>
2019-02-25 14:36:18 +0000

    Fix classname of migration

--- database/migrations/2019_02_25_142656_create_collaborator_invites_table.php
+++ database/migrations/2019_02_25_142656_create_collaborator_invites_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateCollaboratorInviteTokensTable extends Migration
+class CreateCollaboratorInvitesTable extends Migration
 {
     /**
      * Run the migrations.
