10d7fce63eff4276bf3766d0c4b8253be8f9b0c3
Chris Neal <chris@analogrepublic.com>
2019-02-26 09:55:48 +0000

    Fix missing namespace in file model

--- app/Models/File.php
+++ app/Models/File.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use App\Models\Folder;
 use App\Models\Project;
 use App\Models\User;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\MorphMany;
