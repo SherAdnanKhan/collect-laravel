6774451528ff05e675f8594ad882af9075819de9
Craig Childs <childscraig17@gmail.com>
2019-02-28 14:21:11 +0000

    Fix user favourite create and delete mutations, create custom resolvers for them and omit useless input

---
+++ app/Http/GraphQL/Mutations/UserFavourite/Create.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\GraphQL\Mutations\UserFavourite;
+
+use App\Models\File;
+use App\Models\Person;
+use App\Models\Project;
+use App\Models\Recording;
+use App\Models\Session;
+use App\Models\Song;
+use GraphQL\Type\Definition\ResolveInfo;
+use Nuwave\Lighthouse\Exceptions\AuthenticationException;
+use Nuwave\Lighthouse\Support\Contracts\GraphQLContext;
+
+class Create
+{
+    /**
+     * @param $rootValue
+     * @param array $args
+     * @param \Nuwave\Lighthouse\Support\Contracts\GraphQLContext|null $context
+     * @param \GraphQL\Type\Definition\ResolveInfo $resolveInfo
+     * @return array
+     * @throws \Exception
+     */
+    public function resolve($rootValue, array $args, GraphQLContext $context = null, ResolveInfo $resolveInfo)
+    {
+        $input = $args['input'];
+
+        $types = [
+            'file'      => File::class,
+            'project'   => Project::class,
+            'session'   => Session::class,
+            'song'      => Song::class,
+            'person'    => Person::class,
+            'recording' => Recording::class
+        ];
+
+        if (!in_array($input['resource_type'], array_keys($types))) {
+            throw new \Exception('The resource type is not valid');
+        }
+
+        $id = (int) $input['resource_id'];
+        $type = $input['resource_type'];
+
+        $model = $types[$type];
+
+        if (!$model::where('id', $id)->userViewable()->first()) {
+            throw new \Exception('Unable to find resource to favourite');
+        }
+
+        $user = auth()->user();
+        return $user->favourites()->firstOrCreate([
+            'resource_id'   => $id,
+            'resource_type' => $type,
+        ]);
+    }
+}
