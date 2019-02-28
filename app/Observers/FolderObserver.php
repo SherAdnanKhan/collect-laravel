460cb22ba61fea6d5e88ff833f521c31ffd52105
Craig Childs <childscraig17@gmail.com>
2019-02-28 10:54:02 +0000

    Fix cascading trigger of observers

--- app/Observers/FolderObserver.php
+++ app/Observers/FolderObserver.php
@@ -16,7 +16,16 @@ class FolderObserver
     {
         // When we "delete" a folder we should delete all
         // folders inside of it as well as files.
-        $folder->folders()->delete();
-        $folder->files()->delete();
+
+        // Important! We iterate over the children here because we want to
+        // leverage the Observers attached to them.
+
+        $folder->folders()->each(function($subFolder) {
+            $subFolder->delete();
+        });
+
+        $folder->files()->each(function($file) {
+            $file->delete();
+        });
     }
 }
