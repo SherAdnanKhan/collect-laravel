1de05ad4e04f15de00910a0df7139b8abdb6e564
Craig Childs <childscraig17@gmail.com>
2019-03-05 11:49:25 +0000

    Add api token auth guard, schema, nova resourec etc

---
+++ app/Nova/Resources/Application.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Nova\Resources;
+
+use App\Nova\Resource;
+use Illuminate\Http\Request;
+use Laravel\Nova\Fields\Gravatar;
+use Laravel\Nova\Fields\Heading;
+use Laravel\Nova\Fields\ID;
+use Laravel\Nova\Fields\Password;
+use Laravel\Nova\Fields\Select;
+use Laravel\Nova\Fields\Text;
+
+class Application extends Resource
+{
+    /**
+     * The model the resource corresponds to.
+     *
+     * @var string
+     */
+    public static $model = 'App\\Models\\Application';
+
+    /**
+     * The single value that should be used to represent the resource when being displayed.
+     *
+     * @var string
+     */
+    public static $title = 'name';
+
+    /**
+     * The columns that should be searched.
+     *
+     * @var array
+     */
+    public static $search = [
+        'id', 'name'
+    ];
+
+    /**
+     * Get the fields displayed by the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function fields(Request $request)
+    {
+        return [
+            ID::make()->sortable(),
+
+            Text::make('Name')
+                ->sortable()
+                ->rules('required', 'max:255'),
+
+            Text::make('Token', 'api_token')
+                ->sortable()
+                ->help('A secure random string, 60 characters in length')
+                ->rules('required', 'max:60', 'min:60')
+                ->withMeta(['value' => $this->api_token ?? str_random(60)]),
+        ];
+    }
+
+    /**
+     * Get the cards available for the request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function cards(Request $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the filters available for the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function filters(Request $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the lenses available for the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function lenses(Request $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the actions available for the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function actions(Request $request)
+    {
+        return [];
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -14,11 +14,21 @@ use Illuminate\Http\Request;
 */
 
 Route::prefix('api')->group(function () {
+    // We'll use the 'api' guard for multi-part uploads as they'll auth
+    // using the JWT we have to authenticate the user through the GraphQL
+    // portion of the service.
     Route::prefix('multipart-uploads')->middleware('auth:api')->group(function () {
         Route::post('/create', 'MultipartUploads@create');
         Route::post('/prepare', 'MultipartUploads@prepare');
         Route::post('/list', 'MultipartUploads@list');
         Route::post('/abort', 'MultipartUploads@abort');
         Route::post('/complete', 'MultipartUploads@complete');
+    });
+
+
+    // Any routes defined in this group adhere to basic token authentication
+    // for the api routing, using the 'token' guard.
+    Route::middleware('auth:token')->group(function() {
+
     });
 });
