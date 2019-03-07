0debb66562c7564da215ae6703f89ef0e7310d81
Chris Neal <chris@analogrepublic.com>
2019-03-07 09:23:48 +0000

    Add nullable to create venues table migration (ooops)

--- database/migrations/2019_03_06_151810_create_venues_table.php
+++ database/migrations/2019_03_06_151810_create_venues_table.php
@@ -16,12 +16,12 @@ class CreateVenuesTable extends Migration
         Schema::create('venues', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->string('address_1');
-            $table->string('address_2');
-            $table->string('city');
-            $table->string('state');
-            $table->string('zip');
-            $table->string('country');
+            $table->string('address_1')->nullable();
+            $table->string('address_2')->nullable();
+            $table->string('city')->nullable();
+            $table->string('state')->nullable();
+            $table->string('zip')->nullable();
+            $table->string('country')->nullable();
             $table->timestamps();
         });
     }
