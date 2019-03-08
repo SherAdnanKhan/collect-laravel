c1a95cf99ed64b6fd12ce55f9305a6febb40e305
Craig Childs <childscraig17@gmail.com>
2019-03-08 18:11:52 +0000

    Add user viewable scope to only pull users favourites in query

--- app/Models/UserFavourite.php
+++ app/Models/UserFavourite.php
@@ -2,13 +2,17 @@
 
 namespace App\Models;
 
+use App\Contracts\UserAccessible;
 use App\Models\User;
+use App\Traits\UserAccesses;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
 
-class UserFavourite extends Model
+class UserFavourite extends Model implements UserAccessible
 {
+    use UserAccesses;
+
     /**
      * The attributes that are mass assignable.
      *
@@ -34,5 +38,31 @@ class UserFavourite extends Model
     public function favoured(): MorphTo
     {
         return $this->morphTo(null, 'resource_type', 'resource_id');
+    }
+
+    /**
+     * Only the user who's favourite this is can see it.
+     *
+     * @param  Builder $query
+     * @param  array   $data
+     * @return Builder
+     */
+    public function scopeUserViewable(Builder $query, $data = []): Builder
+    {
+        $user = auth()->user();
+        return $query->where('user_id', $user->getAuthIdentifier());
+    }
+
+    /**
+     * Only the user who's favourite this is can delete it.
+     *
+     * @param  Builder $query
+     * @param  array   $data
+     * @return Builder
+     */
+    public function scopeUserDeletable(Builder $query, $data = []): Builder
+    {
+        $user = auth()->user();
+        return $query->where('user_id', $user->getAuthIdentifier());
     }
 }
