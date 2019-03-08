8ea0f600b6c6e706a59ab3f68918d5dea37a733c
Craig Childs <childscraig17@gmail.com>
2019-03-08 18:36:21 +0000

    start setting credit stuff up

---
+++ app/Http/GraphQL/Mutations/Credit/Delete.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\GraphQL\Mutations\Collaborator;
+
+use App\Models\Collaborator;
+use GraphQL\Type\Definition\ResolveInfo;
+use Nuwave\Lighthouse\Exceptions\AuthorizationException;
+use Nuwave\Lighthouse\Support\Contracts\GraphQLContext;
+
+class Delete
+{
+    /**
+     * @param $rootValue
+     * @param array $args
+     * @param \Nuwave\Lighthouse\Support\Contracts\GraphQLContext|null $context
+     * @param \GraphQL\Type\Definition\ResolveInfo $resolveInfo
+     * @return array
+     * @throws \Exception
+     */
+    public function resolve($rootValue, array $args, GraphQLContext $context = null, ResolveInfo $resolveInfo)
+    {
+        $collaborator = Collaborator::where('id', (int) array_get($args, 'input.id'))
+            ->userDeletable()
+            ->first();
+
+        if (!$collaborator) {
+            throw new AuthorizationException('Unable to find collaborator to delete');
+        }
+
+        $collaborator->delete();
+
+        return $collaborator;
+    }
+}
