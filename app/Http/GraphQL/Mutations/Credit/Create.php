7335877ea2e5fbe8514300bb4d3970ce675e483a
Craig Childs <childscraig17@gmail.com>
2019-03-08 18:36:21 +0000

    start setting credit stuff up

---
+++ app/Http/GraphQL/Mutations/Credit/Create.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\GraphQL\Mutations\Credit;
+
+use App\Models\Credit;
+use App\Models\Project;
+use App\Models\User;
+use GraphQL\Type\Definition\ResolveInfo;
+use Nuwave\Lighthouse\Exceptions\AuthorizationException;
+use Nuwave\Lighthouse\Exceptions\GenericException;
+use Nuwave\Lighthouse\Support\Contracts\GraphQLContext;
+
+class Create
+{
+    /**
+     * @param $rootValue
+     * @param array $args
+     * @param \Nuwave\Lighthouse\Support\Contracts\GraphQLContext|null $context
+     * @param \GraphQL\Type\Definition\ResolveInfo $resolveInfo
+     * @return array
+     * @throws \Exception
+     */
+    public function resolve($rootValue, array $args, GraphQLContext $context = null, ResolveInfo $resolveInfo)
+    {
+        $user = auth()->user();
+        $input = array_get($args, 'input');
+        // Todo, complete this mutation.
+    }
+}
