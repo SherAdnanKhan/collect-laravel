39be791e13560d8fd8d022066777fbcb0b7b043e
Craig Childs <childscraig17@gmail.com>
2019-03-08 13:15:37 +0000

    Add missing class use

--- app/Models/Person.php
+++ app/Models/Person.php
@@ -9,6 +9,7 @@ use App\Models\PersonSession;
 use App\Models\Session;
 use App\Models\User;
 use App\Traits\UserAccesses;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
