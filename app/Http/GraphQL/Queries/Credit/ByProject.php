ad51359f9b7fa690f495362391dd39f5a2570ffc
Craig Childs <childscraig17@gmail.com>
2019-03-11 16:43:52 +0000

    Make a start on querying credits by project, may need to relate more tightly to a project, maybe through a many-to-many relationship when we create them

---
+++ app/Http/GraphQL/Queries/Credit/ByProject.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\GraphQL\Queries\Song;
+
+use App\Models\Project;
+use GraphQL\Type\Definition\ResolveInfo;
+use Nuwave\Lighthouse\Exceptions\AuthorizationException;
+use Nuwave\Lighthouse\Support\Contracts\GraphQLContext;
+
+class ByProject
+{
+    /**
+     * @param $rootValue
+     * @param array $args
+     * @param \Nuwave\Lighthouse\Support\Contracts\GraphQLContext|null $context
+     * @param \GraphQL\Type\Definition\ResolveInfo $resolveInfo
+     * @return array
+     * @throws \Exception
+     */
+    public function resolve($rootValue, array $args, GraphQLContext $context = null, ResolveInfo $resolveInfo)
+    {
+        $projectId = (int) $args['projectId'];
+
+        $project = Project::where('id', $projectId)->userViewable()->first();
+
+        if (!$project) {
+            throw new AuthorizationException('The user does not have access to view this projects credits');
+        }
+
+        // TODO:
+        // Fetch all credits for the project,
+        // this is for all all sub-resources.
+    }
+}
--- app/Models/UserFavourite.php
+++ app/Models/UserFavourite.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use App\Contracts\UserAccessible;
 use App\Models\User;
 use App\Traits\UserAccesses;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
