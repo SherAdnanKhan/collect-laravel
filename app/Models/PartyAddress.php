f2dd69a2145a6a1ec2562da19ab676d21094a4d4
Craig Childs <childscraig17@gmail.com>
2019-03-13 17:52:30 +0000

    bring user scoping to address and contact

--- app/Models/PartyAddress.php
+++ app/Models/PartyAddress.php
@@ -2,15 +2,21 @@
 
 namespace App\Models;
 
+use App\Contracts\UserAccessible;
 use App\Models\Party;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use App\Traits\UserAccesses;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 /**
  * Represent a parties addresses.
  */
-class PartyAddress extends Model
+class PartyAddress extends Model implements UserAccessible
 {
+
+    use UserAccesses;
+
     protected $fillable = [
         'party_id', 'line_1', 'line_2', 'line_3',
         'city', 'district', 'postal_code', 'territory_code',
@@ -25,5 +31,47 @@ class PartyAddress extends Model
     public function party(): BelongsTo
     {
         return $this->belongsTo(Party::class);
+    }
+
+    /**
+     * A party address is viewable to everyone.
+     *
+     * @param  Builder $query
+     * @param  array   $data
+     * @return Builder
+     */
+    public function scopeUserViewable(Builder $query, $data = []): Builder
+    {
+        return $query;
+    }
+
+    /**
+     * A user can update a party address if they own it.
+     *
+     * @param  Builder $query
+     * @param  array   $data
+     * @return Builder
+     */
+    public function scopeUserUpdatable(Builder $query, $data = []): Builder
+    {
+        $user = auth()->user();
+        return $query->whereHas('party', function($q) use ($user) {
+            return $q->where('user_id', $user->id);
+        });
+    }
+
+    /**
+     * A user can delete a party address if they own it.
+     *
+     * @param  Builder $query
+     * @param  array   $data
+     * @return Builder
+     */
+    public function scopeUserDeletable(Builder $query, $data = []): Builder
+    {
+        $user = auth()->user();
+        return $query->whereHas('party', function($q) use ($user) {
+            return $q->where('user_id', $user->id);
+        });
     }
 }
