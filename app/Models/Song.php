bc82ba8b1011b86a92bb68ed53d8b4df211b8742
Craig Childs <childscraig17@gmail.com>
2019-03-15 17:14:17 +0000

    Add missing use, remove un-used schema

--- app/Models/Song.php
+++ app/Models/Song.php
@@ -16,6 +16,7 @@ use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\MorphMany;
 
 class Song extends Model implements UserAccessible
