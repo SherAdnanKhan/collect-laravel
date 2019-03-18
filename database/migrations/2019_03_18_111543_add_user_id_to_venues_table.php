70f50dd9c52edb78ad11bac2872f0a9e176ae86b
Craig Childs <childscraig17@gmail.com>
2019-03-18 11:39:30 +0000

    Add migration to add more fields

---
+++ database/migrations/2019_03_18_111543_add_user_id_to_venues_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddUserIdToVenuesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('venues', function (Blueprint $table) {
+            $table->unsignedInteger('user_id')->nullable()->after('id');
+            $table->foreign('user_id')->references('id')->on('users')
+                ->onDelete('set null');
+            $table->boolean('approved')->after('country');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('venues', function (Blueprint $table) {
+            $table->dropForeign(['user_id']);
+            $table->dropColumn('user_id');
+            $table->dropColumn('approved');
+        });
+    }
+}
