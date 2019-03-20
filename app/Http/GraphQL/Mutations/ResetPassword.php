105aa8234627dc3cda04bc35d97ec9bfde32cd51
Craig Childs <childscraig17@gmail.com>
2019-03-20 19:38:18 +0000

    Add payloads, inputs and updated schema. Add reset password mutation class

---
+++ app/Http/GraphQL/Mutations/ResetPassword.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\GraphQL\Mutations;
+
+use App\Models\User;
+use GraphQL\Type\Definition\ResolveInfo;
+use Illuminate\Support\Facades\Password;
+use Nuwave\Lighthouse\Support\Contracts\GraphQLContext;
+use Validator;
+
+class ResetPassword
+{
+    /**
+     * @param $rootValue
+     * @param array $args
+     * @param \Nuwave\Lighthouse\Support\Contracts\GraphQLContext|null $context
+     * @param \GraphQL\Type\Definition\ResolveInfo $resolveInfo
+     * @return array
+     */
+    public function resolve($rootValue, array $args, GraphQLContext $context = null, ResolveInfo $resolveInfo)
+    {
+        $input = array_get($args, 'input');
+
+        $response = $this->broker()->reset($input, function ($user, $password) {
+                $this->resetPassword($user, $password);
+            }
+        );
+
+        return [
+            'success' => $response == Password::PASSWORD_RESET
+        ];
+    }
+
+    private function broker()
+    {
+        return Password::broker();
+    }
+}
