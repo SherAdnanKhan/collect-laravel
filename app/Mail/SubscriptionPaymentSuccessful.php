201f902033832a4a29f0f1a0a4a06b9dba4593ed
Craig Childs <childscraig17@gmail.com>
2019-03-28 23:44:16 +0000

    Add subscriptions templates & mail classes

---
+++ app/Mail/SubscriptionPaymentSuccessful.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Mail;
+
+use App\Models\Subscription;
+use App\Models\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class SubscriptionPaymentSuccessful extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * The user instance
+     *
+     * @var User
+     */
+    protected $user;
+
+    /**
+     * The users subscription which was updated.
+     *
+     * @var Subscription
+     */
+    protected $subscription;
+
+
+    /**
+     * Create a new message instance.
+     *
+     * @param User $user
+     * @return void
+     */
+    public function __construct(User $user, Subscription $subscription)
+    {
+        $this->user = $user;
+        $this->subscription = $subscription;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->view('emails.subscriptions.payment-successful')
+            ->with([
+                'name' => $this->user->name,
+            ]);
+    }
+}
