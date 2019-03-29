62bfd3afe51b748134a33b8a65a9831b90512a7c
Craig Childs <childscraig17@gmail.com>
2019-03-29 22:16:01 +0000

    Add methods and logic to send emails for payment failed & success. As well as plan cancellation.

--- app/Http/Controllers/Webhooks/StripeController.php
+++ app/Http/Controllers/Webhooks/StripeController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Webhooks;
 
 use App\Jobs\Emails\SendSubscriptionCancelledEmail;
+use App\Jobs\Emails\SendSubscriptionPaymentFailedEmail;
+use App\Jobs\Emails\SendSubscriptionPaymentSuccessfulEmail;
 use Laravel\Cashier\Http\Controllers\WebhookController as CashierController;
 
 /**
@@ -33,6 +35,50 @@ class StripeController extends CashierController
 
                 // Dispatch the sending of the cancelled email.
                 SendSubscriptionCancelledEmail::dispatch($user, $subscription);
+            });
+        }
+
+        return new Response('Webhook Handled', 200);
+    }
+
+    /**
+     * Behaviour for payments failed.
+     *
+     * @param  array $payload
+     * @return Response
+     */
+    protected function handleInvoicePaymentFailed(array $payload)
+    {
+        $user = $this->getUserByStripeId($payload['data']['object']['customer']);
+
+        if ($user) {
+            $user->subscriptions->filter(function ($subscription) use ($payload) {
+                return $subscription->stripe_id === $payload['data']['object']['id'];
+            })->each(function ($subscription) {
+                // Dispatch the sending of the payment failed email.
+                SendSubscriptionPaymentFailedEmail::dispatch($user, $subscription);
+            });
+        }
+
+        return new Response('Webhook Handled', 200);
+    }
+
+    /**
+     * Behaviour for payments failed.
+     *
+     * @param  array $payload
+     * @return Response
+     */
+    protected function handleInvoicePaymentSucceeded(array $payload)
+    {
+        $user = $this->getUserByStripeId($payload['data']['object']['customer']);
+
+        if ($user) {
+            $user->subscriptions->filter(function ($subscription) use ($payload) {
+                return $subscription->stripe_id === $payload['data']['object']['id'];
+            })->each(function ($subscription) {
+                // Dispatch the sending of the payment success email.
+                SendSubscriptionPaymentSuccessfulEmail::dispatch($user, $subscription);
             });
         }
 
