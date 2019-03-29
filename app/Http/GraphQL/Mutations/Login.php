d1d09e19f087bff6f0f22ebab299ffa532392b76
Chris Neal <chris@analogrepublic.com>
2019-03-29 16:12:34 +0000

    Add refresh token mutation

--- app/Http/GraphQL/Mutations/Login.php
+++ app/Http/GraphQL/Mutations/Login.php
@@ -5,6 +5,7 @@ namespace App\Http\GraphQL\Mutations;
 use GraphQL\Type\Definition\ResolveInfo;
 use Nuwave\Lighthouse\Exceptions\AuthenticationException;
 use Nuwave\Lighthouse\Support\Contracts\GraphQLContext;
+use Tymon\JWTAuth\Facades\JWTAuth;
 
 class Login
 {
@@ -30,5 +31,32 @@ class Login
             'token_type'   => 'bearer',
             'expires_in'   => auth()->factory()->getTTL() * 60
         ];
+    }
+
+    /**
+     * @param $rootValue
+     * @param array $args
+     * @param \Nuwave\Lighthouse\Support\Contracts\GraphQLContext|null $context
+     * @param \GraphQL\Type\Definition\ResolveInfo $resolveInfo
+     * @return array
+     * @throws \Exception
+     */
+    public function refresh($rootValue, array $args, GraphQLContext $context = null, ResolveInfo $resolveInfo)
+    {
+        try {
+            $refreshed = JWTAuth::refresh(JWTAuth::getToken());
+
+            return [
+                'access_token' => $refreshed,
+                'token_type'   => 'bearer',
+                'expires_in'   => auth()->factory()->getTTL() * 60
+            ];
+        } catch (JWTException $e) {
+            return [
+                'access_token' => '',
+                'token_type'   => 'bearer',
+                'expires_in'   => 0
+            ];
+        }
     }
 }
