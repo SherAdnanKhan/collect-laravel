f2936e370c768669dd1cab7ce8a895e5e3a22422
Craig Childs <childscraig17@gmail.com>
2019-03-29 22:08:43 +0000

    Send cancelled email when we cancel a subscription

--- app/Http/Controllers/Webhooks/StripeController.php
+++ app/Http/Controllers/Webhooks/StripeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Webhooks;
 
+use App\Jobs\Emails\SendSubscriptionCancelledEmail;
 use Laravel\Cashier\Http\Controllers\WebhookController as CashierController;
 
 /**
@@ -11,4 +12,30 @@ class StripeController extends CashierController
 {
     // Override methods from the cashier controller when we want to do
     // something different.
+
+
+    /**
+     * Handle a cancelled customer from a Stripe subscription.
+     *
+     * @override
+     * @param  array  $payload
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    protected function handleCustomerSubscriptionDeleted(array $payload)
+    {
+        $user = $this->getUserByStripeId($payload['data']['object']['customer']);
+
+        if ($user) {
+            $user->subscriptions->filter(function ($subscription) use ($payload) {
+                return $subscription->stripe_id === $payload['data']['object']['id'];
+            })->each(function ($subscription) {
+                $subscription->markAsCancelled();
+
+                // Dispatch the sending of the cancelled email.
+                SendSubscriptionCancelledEmail::dispatch($user, $subscription);
+            });
+        }
+
+        return new Response('Webhook Handled', 200);
+    }
 }
