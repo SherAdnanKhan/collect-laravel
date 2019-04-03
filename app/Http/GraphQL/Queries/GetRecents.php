cfeb6a33324b9e22904a5ff7670aadf726e228b8
Craig Childs <childscraig17@gmail.com>
2019-04-03 16:14:07 +0100

    Only get recents where there's a resource

--- app/Http/GraphQL/Queries/GetRecents.php
+++ app/Http/GraphQL/Queries/GetRecents.php
@@ -23,15 +23,15 @@ class GetRecents
         $count = array_get($args, 'count', 5);
         $user = auth()->user();
 
-        $query = $user->eventLogs();
+        $query = $user->eventLogs()->whereHas('resource');
 
         if (array_key_exists('resourceType', $args) && in_array(array_get($args, 'resourceType'), EventLog::TYPES)) {
-            $query = $query->where('resource_type', array_get($args, 'resourceType'));
+            $query = $query->where('event_logs.resource_type', array_get($args, 'resourceType'));
         } else {
-            $query = $query->where('resource_type', '!=', 'collaborator');
+            $query = $query->where('event_logs.resource_type', '!=', 'collaborator');
         }
 
-        return $query->where('resource_type', '!=', 'comment')
+        return $query->where('event_logs.resource_type', '!=', 'comment')
             ->where('event_logs.action', '<>', 'delete')
             ->latest('event_logs.updated_at')
             ->latest('event_logs.created_at')
