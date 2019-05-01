a962a1010efc9b47368969bf2aed46ae29092541
Chris Neal <chris@analogrepublic.com>
2019-05-01 12:11:24 +0100

    Add outstanding user invites query

---
+++ app/Http/GraphQL/Queries/GetInvites.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\GraphQL\Queries;
+
+use App\Models\CollaboratorInvite;
+use GraphQL\Type\Definition\ResolveInfo;
+use Nuwave\Lighthouse\Exceptions\AuthorizationException;
+use Nuwave\Lighthouse\Support\Contracts\GraphQLContext;
+
+class GetInvites
+{
+    /**
+     * @param $rootValue
+     * @param array $args
+     * @param \Nuwave\Lighthouse\Support\Contracts\GraphQLContext|null $context
+     * @param \GraphQL\Type\Definition\ResolveInfo $resolveInfo
+     * @return array
+     * @throws \Exception
+     */
+    public function resolve($rootValue, array $args, GraphQLContext $context = null, ResolveInfo $resolveInfo)
+    {
+        $user = auth()->user();
+
+        return CollaboratorInvite::whereNull('user_id')->whereHas('collaborator', function($query) use($user) {
+            return $query->where('email', $user->email);
+        })->get();
+    }
+}
