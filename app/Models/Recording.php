9abc06ef202081210b6858d52df807de2b97d917
Chris Neal <chris@analogrepublic.com>
2019-05-17 15:56:07 +0100

    Remove version text from recording

--- app/Models/Recording.php
+++ app/Models/Recording.php
@@ -47,8 +47,7 @@ class Recording extends Model implements UserAccessible, EventLoggable, Creditab
     protected $fillable = [
         'project_id', 'party_id', 'party_role_id', 'song_id', 'name', 'recording_type_id', 'description',
         'isrc', 'subtitle', 'version_type_id', 'version_type_user_defined_value', 'recorded_on', 'mixed_on', 'duration',
-        'language', 'key_signature', 'time_signature', 'tempo', 'recording_type_user_defined_value', 'party_role_user_defined_value',
-        'version'
+        'language', 'key_signature', 'time_signature', 'tempo', 'recording_type_user_defined_value', 'party_role_user_defined_value'
     ];
 
     protected $casts = [
@@ -206,7 +205,7 @@ class Recording extends Model implements UserAccessible, EventLoggable, Creditab
      *
      * @return BelongsTo
      */
-    public function versionType(): BelongsTo
+    public function version(): BelongsTo
     {
         return $this->belongsTo(VersionType::class, 'version_type_id');
     }
