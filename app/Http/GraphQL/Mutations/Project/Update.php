4d285b2d8821bd6b25d4faea2bfc520f50aac4be
Chris Neal <chris@analogrepublic.com>
2019-05-20 15:59:36 +0100

    Move project number unique check into resolver

--- app/Http/GraphQL/Mutations/Project/Update.php
+++ app/Http/GraphQL/Mutations/Project/Update.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\GraphQL\Mutations\Project;
 
+use App\Http\GraphQL\Exceptions\ValidationException;
 use App\Models\Project;
 use GraphQL\Type\Definition\ResolveInfo;
 use Nuwave\Lighthouse\Exceptions\AuthorizationException;
@@ -27,6 +28,12 @@ class Update
 
         if (!$project) {
             throw new AuthorizationException('Unable to find project to update');
+        }
+
+        if ($input['number'] != $project->number) {
+            if (Project::where('number', 'like', $project->number)->count() > 0) {
+                throw new ValidationException('Project Number must be unique.');
+            }
         }
 
         $saved = $project->fill($input)->save();
