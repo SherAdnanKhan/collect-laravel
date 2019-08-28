a096a7cd1e8194744e555552fde2748f58df0cb9
Craig Childs <childscraig17@gmail.com>
2019-08-28 22:17:38 +0100

    Migration

--- database/migrations/2019_08_28_195743_alter_party_address_country_field.php
+++ database/migrations/2019_08_28_195743_alter_party_address_country_field.php
@@ -13,7 +13,12 @@ class AlterPartyAddressCountryField extends Migration
      */
     public function up()
     {
-        //
+        Schema::table('party_addresses', function (Blueprint $table) {
+            $table->dropColumn('territory_code');
+            $table->unsignedInteger('country_id')->after('postal_code')->nullable()->default(1);
+            $table->foreign('country_id')->references('id')->on('countries')
+                ->onDelete('set null');
+        });
     }
 
     /**
@@ -23,6 +28,10 @@ class AlterPartyAddressCountryField extends Migration
      */
     public function down()
     {
-        //
+        Schema::table('party_addresses', function (Blueprint $table) {
+            $table->dropForeign(['country_id']);
+            $table->dropColumn('country_id');
+            $table->string('territory_code', 255)->after('postal_code');
+        });
     }
 }
