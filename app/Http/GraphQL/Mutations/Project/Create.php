4530ecb9a783d7d6824070abb1ffb9bb755f1564
Craig Childs <childscraig17@gmail.com>
2019-09-17 10:06:57 +0100

    Oh now it's right

--- app/Http/GraphQL/Mutations/Project/Create.php
+++ app/Http/GraphQL/Mutations/Project/Create.php
@@ -5,7 +5,7 @@ namespace App\Http\GraphQL\Mutations\Project;
 use App\Models\Project;
 use GraphQL\Type\Definition\ResolveInfo;
 use Nuwave\Lighthouse\Exceptions\GenericException;
-use Nuwave\Lighthouse\Exceptions\ValidationException;
+use App\Http\GraphQL\Exceptions\ValidationException;
 use Nuwave\Lighthouse\Exceptions\AuthorizationException;
 use Nuwave\Lighthouse\Support\Contracts\GraphQLContext;
 
