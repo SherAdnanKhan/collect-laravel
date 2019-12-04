ee906a374d6aeb2bb292ab55e375e00b5c0dbcfc
Craig Childs <childscraig17@gmail.com>
2019-12-04 21:21:08 +0000

    More debug for 2FA

--- app/Jobs/SMS/SendTwoFactorSMS.php
+++ app/Jobs/SMS/SendTwoFactorSMS.php
@@ -39,11 +39,12 @@ class SendTwoFactorSMS implements ShouldQueue
      */
     public function handle()
     {
-        Log::debug(sprintf('Sending code: %s to phone: %s', $this->code, $this->phone));
+        $from = config('services.nexmo.from');
+        Log::debug(sprintf('Sending code: %s to phone: %s from: %s', $this->code, $this->phone, $from));
 
         $client = resolve('Nexmo\Client');
         $message = 'Your 2FA Code is: %s';
-        $text = new Text($this->phone, config('services.nexmo.from'), sprintf($message, $this->code));
+        $text = new Text($this->phone, $from, sprintf($message, $this->code));
         // $text->setClientRef('2fa-' . $this->phone);
         //     ->setClass(Text::CLASS_FLASH);
 
