ba2be34d719c8624f32a6ff9e3205c062781dc4c
Chris Neal <chris@analogrepublic.com>
2020-02-28 13:40:32 +0000

    Resize limits once a file is deleted

--- app/Observers/FileObserver.php
+++ app/Observers/FileObserver.php
@@ -48,4 +48,35 @@ class FileObserver
             SendUserStorageLimitReachedEmail::dispatch($user);
         }
     }
+
+    /**
+     * Handle the folder "deleted" event.
+     *
+     * @param  \App\Models\File  $file
+     * @return void
+     */
+    public function deleted(File $file)
+    {
+        $project = null;
+        if ($file->project_id) {
+            $project = $file->project;
+            $user = $project->user()->first();
+        } else {
+            $user = $file->user()->first();
+        }
+
+        if ($project) {
+            $project->update([
+                'total_storage_used' => DB::raw('total_storage_used - ' . (int)$file->size)
+            ]);
+        }
+
+        $user->update([
+            'total_storage_used' => DB::raw('total_storage_used - ' . (int)$file->size)
+        ]);
+
+        $user->refresh();
+
+        Subscription::broadcast('userStorageUpdated', $user);
+    }
 }
