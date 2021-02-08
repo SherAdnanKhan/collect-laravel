90461019d55c6a6b32d5676f1fbc5f7607f9dc2f
Mohsin <abdul.mohsin.malik@gmail.com>
2021-02-08 19:08:34 +0500

    validate and download share tempurl expiry fixed

--- app/Http/GraphQL/Queries/ValidateShare.php
+++ app/Http/GraphQL/Queries/ValidateShare.php
@@ -4,6 +4,7 @@ namespace App\Http\GraphQL\Queries;
 
 use App\Models\Share;
 use App\Models\ShareUser;
+use Carbon\Carbon;
 use GraphQL\Type\Definition\ResolveInfo;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Storage;
@@ -59,7 +60,7 @@ class ValidateShare
 
         if (isset($share->expires_at)) {
             $url = Storage::disk('s3')->temporaryUrl(
-                substr($share->path, 1), 7
+                substr($share->path, 1), Carbon::now()->addDay()
             );
         } else {
             $url = Storage::disk('s3')->url(substr($share->path, 1));
