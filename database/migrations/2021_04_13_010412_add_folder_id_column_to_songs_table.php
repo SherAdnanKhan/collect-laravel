378e9de5046e7c4204394aeeee60fb40f1cb9de4
Mohsin <abdul.mohsin.malik@gmail.com>
2021-04-13 14:07:02 +0500

    VC-121 - add files to recording feature added

---
+++ database/migrations/2021_04_13_010412_add_folder_id_column_to_songs_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddFolderIdToSongsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('songs', function (Blueprint $table) {
+            $table->unsignedInteger('folder_id')->nullable()->after('song_type_id');
+            $table->foreign('folder_id')->references('id')->on('folders')
+                ->onDelete('set null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('songs', function (Blueprint $table) {
+            $table->dropForeign(['folder_id']);
+            $table->dropColumn(['folder_id']);
+        });
+    }
+}
